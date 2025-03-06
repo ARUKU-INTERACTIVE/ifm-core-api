d1940aebf7b50d6dcc8543f78307290c1fdc8c44
lucianocolin <lucianocolin99@gmail.com>
2025-03-06 16:22:48 -0300

    Modify incorrect signature error message

--- src/module/stellar/application/exceptions/stellar.error.ts
+++ src/module/stellar/application/exceptions/stellar.error.ts
@@ -1,5 +1,5 @@
 export enum STELLAR_ERROR {
   INVALID_PUBLIC_KEY = 'Invalid public key',
   INCORRECT_MEMO = 'Incorrect memo',
-  INCORRECT_SIGN = 'Incorrect sign',
+  INCORRECT_SIGN = 'Incorrect signature',
 }
