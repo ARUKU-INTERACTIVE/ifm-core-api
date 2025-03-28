a9e01fa32548c0f3fd34038ca4a9817464fb00fa
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2025-03-28 17:32:45 -0300

    feat: add player mintPLayer tests

---
+++ src/module/player/__test__/player.e2e.spec.ts
@@ -0,0 +1,104 @@
+import { ICreatePlayerDto } from '@module/player/application/dto/create-player.dto.interface';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+
+import { loadFixtures } from '@data/util/fixture-loader';
+
+import {
+  InvalidStellarTransactionError,
+  StellarAccountNotFound,
+} from '@common/infrastructure/stellar/exception/stellar.exception';
+
+import { setupApp } from '@config/app.config';
+import { datasourceOptions } from '@config/orm.config';
+
+import { testModuleBootstrapper } from '@test/test.module.bootstrapper';
+import { createAccessToken } from '@test/test.util';
+
+describe('Player Module', () => {
+  let app: INestApplication;
+
+  const adminToken = createAccessToken({
+    publicKey: 'GXXX-XXXX-XXXX-XXXX1',
+    sub: '00000000-0000-0000-0000-00000000000X',
+  });
+
+  beforeAll(async () => {
+    await loadFixtures(`${__dirname}/fixture`, datasourceOptions);
+    const moduleRef = await testModuleBootstrapper();
+    app = moduleRef.createNestApplication({ logger: false });
+
+    setupApp(app);
+
+    await app.init();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST - /player', () => {
+    const createPlayerDto = {
+      name: 'Jonh Doe',
+      metadataUri: 'http://example.com',
+    } as ICreatePlayerDto;
+    it('Should return the XDR of the mintPlayer transaction.', async () => {
+      await request(app.getHttpServer())
+        .post('/api/v1/player/mint')
+        .auth(adminToken, { type: 'bearer' })
+        .send(createPlayerDto)
+        .expect(HttpStatus.CREATED)
+        .then(({ body }) => {
+          console.log(body, 'body');
+          const expectedResponse = expect.objectContaining({
+            data: expect.objectContaining({
+              attributes: expect.objectContaining({
+                xdr: expect.any(String),
+              }),
+            }),
+          });
+          expect(expectedResponse).toEqual(body);
+        });
+    });
+    it('Should return an error message stating that the Stellar account was not found', async () => {
+      const publicKey = 'ERROR';
+      const adminToken = createAccessToken({
+        publicKey,
+        sub: '00000000-0000-0000-0000-0000000000XX',
+      });
+
+      await request(app.getHttpServer())
+        .post('/api/v1/player/mint')
+        .auth(adminToken, { type: 'bearer' })
+        .send(createPlayerDto)
+        .expect(HttpStatus.NOT_FOUND)
+        .then(({ body }) => {
+          expect(body.error.detail).toEqual(
+            new StellarAccountNotFound(publicKey).message,
+          );
+        });
+    });
+
+    it('Should return an error indicating that there was a failure in the transaction.', async () => {
+      const publicKey = 'ERROR-ACCOUNT';
+      const adminToken = createAccessToken({
+        publicKey,
+        sub: '00000000-0000-0000-0000-000000000XXX',
+      });
+      await request(app.getHttpServer())
+        .post('/api/v1/player/mint')
+        .auth(adminToken, { type: 'bearer' })
+        .send(createPlayerDto)
+        .expect(HttpStatus.BAD_REQUEST)
+        .then(({ body }) => {
+          expect(body.error.detail).toEqual(
+            new InvalidStellarTransactionError().message,
+          );
+        });
+    });
+  });
+});
--- src/test/test.module.bootstrapper.ts
+++ src/test/test.module.bootstrapper.ts
@@ -2,6 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 
 import {
+  InvalidStellarTransactionError,
+  StellarAccountNotFound,
+} from '@common/infrastructure/stellar/exception/stellar.exception';
+import { SorobanContractAdapter } from '@common/infrastructure/stellar/soroban-contract.adapter';
+import { StellarAccountAdapter } from '@common/infrastructure/stellar/stellar-account.adapter';
+
+import {
   IDENTITY_PROVIDER_SERVICE_KEY,
   IIdentityProviderService,
 } from '@iam/authentication/application/service/identity-provider.service.interface';
@@ -33,6 +40,27 @@ export const testModuleBootstrapper = (): Promise<TestingModule> => {
   })
     .overrideProvider(IDENTITY_PROVIDER_SERVICE_KEY)
     .useValue(identityProviderServiceMock)
+    .overrideProvider(StellarAccountAdapter)
+    .useValue({
+      createIssuerKeypair: jest.fn().mockReturnValue({
+        publicKey: jest.fn(),
+      }),
+      getAccount: jest.fn().mockImplementation((publicKey) => {
+        if (publicKey === 'ERROR') {
+          throw new StellarAccountNotFound(publicKey);
+        }
+        return publicKey;
+      }),
+    })
+    .overrideProvider(SorobanContractAdapter)
+    .useValue({
+      mintPlayer: jest.fn().mockImplementation((account) => {
+        if (account === 'ERROR-ACCOUNT') {
+          throw new InvalidStellarTransactionError();
+        }
+        return 'xdr';
+      }),
+    })
     .overrideProvider(StellarService)
     .useValue(stellarServiceMock)
     .compile();
