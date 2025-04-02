ce7e68284644d8c429ac4764c3926e99db59d14a
lucianocolin <lucianocolin99@gmail.com>
2025-04-01 21:15:50 -0300

    Add endpoint to get all players

--- src/module/player/interface/player.controller.ts
+++ src/module/player/interface/player.controller.ts
@@ -1,10 +1,16 @@
 import { CreatePlayerDto } from '@module/player/application/dto/create-player.dto';
+import { PlayerFieldsQueryParamsDto } from '@module/player/application/dto/params/player-fields-query-params.dto';
+import { PlayerFilterQueryParamsDto } from '@module/player/application/dto/params/player-filter-query-params.dto';
+import { PlayerIncludeQueryParamsDto } from '@module/player/application/dto/params/player-include-query-params.dto';
+import { PlayerSortQueryParamsDto } from '@module/player/application/dto/params/player-sort-query-params.dto';
 import { PlayerResponseDto } from '@module/player/application/dto/player-response.dto';
 import { PlayerService } from '@module/player/application/service/player.service';
 import { PLAYER_ENTITY_NAME } from '@module/player/domain/player.name';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
+import { ManySerializedResponseDto } from '@common/base/application/dto/many-serialized-response.dto';
 import { OneSerializedResponseDto } from '@common/base/application/dto/one-serialized-response.dto';
+import { PageQueryParamsDto } from '@common/base/application/dto/page-query-params.dto';
 import { ControllerEntity } from '@common/base/application/interface/decorators/endpoint-entity.decorator';
 import { TransactionXDRResponseDto } from '@common/infrastructure/stellar/dto/transaction-xdr-response.dto';
 import { TransactionXDRDTO } from '@common/infrastructure/stellar/dto/transaction-xdr.dto';
@@ -19,6 +25,27 @@ import { User } from '@iam/user/domain/user.entity';
 @Controller('/player')
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
+
+  @Get()
+  getAll(
+    @Query('page') page: PageQueryParamsDto,
+    @Query('filter') filter: PlayerFilterQueryParamsDto,
+    @Query('fields') fields: PlayerFieldsQueryParamsDto,
+    @Query('sort') sort: PlayerSortQueryParamsDto,
+    @Query('include') include: PlayerIncludeQueryParamsDto,
+    @CurrentUser() user: User,
+  ): Promise<ManySerializedResponseDto<PlayerResponseDto>> {
+    return this.playerService.getAll(
+      {
+        page,
+        filter,
+        sort,
+        fields: fields.target,
+        include: include.target ?? [],
+      },
+      user,
+    );
+  }
 
   @Get('/:id')
   getPlayerById(@Param('id') id: number): Promise<PlayerResponseDto> {
