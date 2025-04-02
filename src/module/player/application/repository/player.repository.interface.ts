910e81017f0ae4e1c1a58d66847897cc41582264
lucianocolin <lucianocolin99@gmail.com>
2025-04-01 21:21:58 -0300

    Add get all method to player repository

--- src/module/player/application/repository/player.repository.interface.ts
+++ src/module/player/application/repository/player.repository.interface.ts
@@ -1,9 +1,15 @@
 import { PlayerRelation } from '@module/player/application/enum/player-relations.enum';
 import { Player } from '@module/player/domain/player.domain';
 
+import { ICollection } from '@common/base/application/dto/collection.interface';
+import { IGetAllOptions } from '@common/base/application/interface/get-all-options.interface';
+
 export const PLAYER_REPOSITORY_KEY = 'player_repository';
 
 export interface IPlayerRepository {
+  getAll(
+    options: IGetAllOptions<Player, PlayerRelation[]>,
+  ): Promise<ICollection<Player>>;
   getOneById(id: number, relations?: PlayerRelation[]): Promise<Player>;
   saveOne(book: Player, relations?: PlayerRelation[]): Promise<Player>;
 }
--- src/module/player/infrastructure/database/player.mysql.repository.ts
+++ src/module/player/infrastructure/database/player.mysql.repository.ts
@@ -5,11 +5,37 @@ import { PlayerSChema } from '@module/player/infrastructure/database/player.sche
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { ICollection } from '@common/base/application/dto/collection.interface';
+import { IGetAllOptions } from '@common/base/application/interface/get-all-options.interface';
+
 export class PlayerRepository implements IPlayerRepository {
   constructor(
     @InjectRepository(PlayerSChema)
     private readonly repository: Repository<Player>,
   ) {}
+
+  async getAll(
+    options: IGetAllOptions<Player, PlayerRelation[]>,
+  ): Promise<ICollection<Player>> {
+    const { filter, page, sort, fields, include } = options || {};
+
+    const [items, itemCount] = await this.repository.findAndCount({
+      where: { ...filter },
+      order: sort,
+      select: fields,
+      take: page.size,
+      skip: page.offset,
+      relations: include,
+    });
+
+    return {
+      data: items,
+      pageNumber: page.number,
+      pageSize: page.size,
+      pageCount: Math.ceil(itemCount / page.size),
+      itemCount,
+    };
+  }
 
   async getOneById(id: number, relations?: PlayerRelation[]): Promise<Player> {
     return await this.repository.findOne({
