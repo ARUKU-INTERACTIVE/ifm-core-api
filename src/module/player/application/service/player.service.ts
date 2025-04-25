3eebf03bc4835b1577ee4585ce84c8bae65c5ea4
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2025-04-25 13:03:41 -0300

    feat: add FormationModule in imports of PlayerModule

--- src/module/player/application/service/player.service.ts
+++ src/module/player/application/service/player.service.ts
@@ -1,3 +1,4 @@
+import { FormationPlayerService } from '@module/formation-player/application/service/formation-player.service';
 import { PlayerResponseAdapter } from '@module/player/application/adapter/player-response.adapter';
 import { AddPlayerToRosterDto } from '@module/player/application/dto/add-player-roster.dto';
 import { UpdatePlayerResponseDto } from '@module/player/application/dto/player-response-update-dto';
@@ -54,6 +55,7 @@ export class PlayerService {
     private readonly stellarTransactionAdapter: StellarTransactionAdapter,
     private readonly sorobanContractAdapter: SorobanContractAdapter,
     private readonly stellarAccountAdapter: StellarAccountAdapter,
+    private readonly formationPlayerService: FormationPlayerService,
     private readonly pinataAdapter: PinataAdapter,
     @Inject(forwardRef(() => RosterService))
     private readonly rosterService: RosterService,
@@ -319,7 +321,7 @@ export class PlayerService {
     const roster = await this.rosterService.getOneRosterOrFail({
       uuid: updatePlayerRosterDto.rosterUuid,
     });
-
+    await this.formationPlayerService.deleteManyByPlayerIdOrFail(player.id);
     const team = await this.teamService.getOneByUserIdOrFail(user.id, [
       TeamRelation.ROSTER_ENTITY,
     ]);
