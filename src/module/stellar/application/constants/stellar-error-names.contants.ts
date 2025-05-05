fafc36c8b8753e7da1160e9b7e91cd41be81826a
ElectroCERP <carloscer27r@gmail.com>
2025-05-05 10:01:56 -0300

    Add new Stellar Errors messages to use when handling challenge transaction creation

---
+++ src/module/stellar/application/constants/stellar-error-names.contants.ts
@@ -0,0 +1,1 @@
+export const STELLAR_NOT_FOUND_ERROR = 'NotFoundError';
--- src/module/stellar/application/exceptions/stellar.error.ts
+++ src/module/stellar/application/exceptions/stellar.error.ts
@@ -2,4 +2,6 @@ export enum STELLAR_ERROR {
   INVALID_PUBLIC_KEY = 'Invalid public key',
   INCORRECT_MEMO = 'Incorrect memo',
   INCORRECT_SIGN = 'Incorrect signature',
+  ERROR_BUILDING_TRANSACTION = 'Error building transaction',
+  ACCOUNT_NOT_FOUND = 'Account not found in the Stellar network',
 }
